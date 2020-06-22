deb7c877509d70ff71c6ebc35489761f28f26a3a
Anandu <anandhuap700@gmail.com>
2020-06-22 19:41:43 +0530

    Static method program added

---
+++ src/Final.java
@@ -0,0 +1,7 @@
+
+public class Final {
+	void displayfinal(){
+		System.out.println("Hai dear how are you?");
+	}
+
+}
---
+++ src/HowAreYou.java
@@ -0,0 +1,10 @@
+
+public class HowAreYou {
+	public static void main(String args[])
+	{
+		First f=new First();
+		f.displayFirst();
+		
+	}
+
+}
