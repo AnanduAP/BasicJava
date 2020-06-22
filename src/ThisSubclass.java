510cf4ab020ab3120f63b3d9e69f4b0f93e10cc5
Anandu <anandhuap700@gmail.com>
2020-06-22 20:06:30 +0530

    This programs are added

---
+++ src/ThisSubclass.java
@@ -0,0 +1,10 @@
+
+public class ThisSubclass {
+int a,b;
+ThisSubclass(int a,int b){
+	this.a=a;
+	this.b=b;
+	System.out.println("Number 1 is: " +a);
+	System.out.println("Number 2 is: " +b);
+}
+}
---
+++ src/Thismain.java
@@ -0,0 +1,6 @@
+
+public class Thismain {
+public static void main(String args[]){
+	ThisSubclass s=new ThisSubclass(10,12);
+}
+}
