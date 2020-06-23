750b740f4ac91481fc31d8141127adaaafd2d47f
Anandu <anandhuap700@gmail.com>
2020-06-23 15:50:38 +0530

    Method overloading program example added

---
+++ src/MainOverload.java
@@ -0,0 +1,8 @@
+
+public class MainOverload {
+public static void main(String args[]){
+	SubOverload over=new SubOverload();
+	over.display();
+	over.display(10);
+}
+}
---
+++ src/SubOverload.java
@@ -0,0 +1,9 @@
+
+public class SubOverload {
+void display(){
+	System.out.println("Hai");
+}
+void display(int a){
+	System.out.println("Hello anandhu number is :"+a);
+}
+}
